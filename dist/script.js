9ebcbc6a2f50a8e77a6aabe86cc7cf1da08289bd
Kamal Paudel <techitus07@gmail.com>
2023-12-28 19:48:47 +0545

    Fourth commit

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-function checkThala() {
+function checkThala(tabName) {
    
     //Fetch the input
     let userInput = document.getElementById('inputField').value.trim();
@@ -13,18 +13,51 @@ function checkThala() {
     let sum = digitArray.reduce((a , b) => a + b, 0)
     
     //To check condition
-    if(sum === 7){
-        alert('Thala for a reason')
+        if (sum == 7) {
+            confettiAnimation();
+            Swal.fire({
+                title: "The Message is clear!!!",
+                text: "Thala For A Reason!",
+                html: '<video autoplay muted loop class="text-center"><source src="/assests/correct.mp4" type="video/mp4"></video>',       
+                 showCloseButton: true,
+                focusConfirm: true,
+                confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+                confirmButtonAriaLabel: 'OK',
+            });
+        } 
 
+        else {
+            Swal.fire({
+                title: "Wrong!",
+                text: "Not Thala For A Reason!",
+                imageUrl: "/assests/gg.jpg",
+                imageWidth: 400,
+                imageHeight: 250,
+                imageAlt: "Not Thala"
+            });
+            
+        }
     }
-   else  if(userInput.length === 7 || userInput === "7"){
-        alert("Thala for a reason")
+    
+    function confettiAnimation() {
+        let duration = 5 * 1000;
+        let animationEnd = Date.now() + duration;
+        let defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+    
+        function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+        }
+    
+        let interval = setInterval(function() {
+            let timeLeft = animationEnd - Date.now();
+    
+            if (timeLeft <= 0) {
+                return clearInterval(interval);
+            }
+    
+            let particleCount = 50 * (timeLeft / duration);
+            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+        }, 250);
     }
-       
-    else{
-alert("ufs")
-
-    }
-    //To reset the user input
-    inputField.value = "";
-    }
+    
